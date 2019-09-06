a1bb621896f4846c8ddf24d2bcff97346608fe5f
betheuil <43854404+betheuil@users.noreply.github.com>
2019-09-06 12:57:40 +0100

    Update earthquakeWDC.js

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -11,3 +11,9 @@
 
     tableau.registerConnector(myConnector);
 })();
+$(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.submit();
+    });
+});
